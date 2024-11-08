92b0e33aadea682d5df5de16e006d48c7298a71d
jonh-carvalho <jonh.carvalho@professores.ibmec.edu.br>
2024-11-08 07:38:23 -0300

    update

--- src/content_app/views.py
+++ src/content_app/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
-
+from rest_framework import generics
 from rest_framework import viewsets
 from .models import Content
 from .serializers import ContentSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 class ContentViewSet(viewsets.ModelViewSet):
-    queryset = Content.objects.all()
     serializer_class = ContentSerializer
+    queryset = Content.objects.all()
     #permission_classes = [IsAuthenticatedOrReadOnly]
 
-    #def perform_create(self, serializer):
-     #   serializer.save(creator=self.request.user)# Create your views here.
+    def get_queryset(self):
+        queryset = Content.objects.all()
+        content_type = self.request.query_params.get('content_type')
+        
+        if content_type in [choice[0] for choice in Content.CONTENT_TYPES]:
+            queryset = queryset.filter(content_type=content_type)
+
+        return queryset
+        
